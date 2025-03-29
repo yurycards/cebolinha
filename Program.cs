f6621ccbd8abda9e5d46648fdfe0c0cfa06dba64
yuri cardoso <yurycards@gmail.com>
2025-03-28 22:30:28 -0300

    atividae feita

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+string nomedigitado;
+string nomeleet;
+Console.WriteLine();
+nomedigitado= Console.ReadLine() ! ;
+nomeleet = nomedigitado.Replace("R","L").
+                        Replace("r","l") ;
+Console.WriteLine($" {nomeleet}!");
+
+
+
